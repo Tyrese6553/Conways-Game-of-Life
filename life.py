27eac578aabbb5ac7e3f7bacb54f51192003051e
Tyrese6553 <tmathenlall@gmail.com>
2025-10-17 19:46:21 +0200

    Corrected random_state function to accept height param

--- life.py
+++ life.py
@@ -7,9 +7,31 @@ def random_state(width, height):
     for _ in range(width):
         value = random.choice([0,1])
         board1.append(value)
+   
+    for _ in range(height):
         board2.append(board1)
 
     return board2
 
 
-print(random_state(5,5))
+
+'''
+def render(board):
+    new_board1 = []
+    new_board2 = []
+
+    for num in board:
+        for nest in num:
+            if nest == 0:
+                new_board1.append(" ")
+            if nest == 1:
+                new_board1.append("#")
+
+    return new_board1
+
+board = [[1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0]]
+
+print(render(board))
+'''
+
+    # use \n to print new line after each element from list

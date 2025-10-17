c3fa4bba717f0b9cc8a1d57aa977901b89dcaae1
Tyrese Mathenlall <Tyrese.Mathenlall@gppos.net>
2025-10-17 18:47:07 +0200

    Created dead state with only width

--- life.py
+++ life.py
@@ -1,2 +1,19 @@
+def dead_state(width, height=0):
+    board1 = []
+    board2 = []
 
-# testing git
+    # For width
+    for _ in range(width):
+        board1.append(0)
+        board2.append(board1)
+
+    return board2
+
+
+    
+print(dead_state(5))
+
+    # [
+    # [0,0,0,0,0], 
+    # [0,0,0,0,0]
+    # ]

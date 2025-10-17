dc0c41467bf25f27daf30c34d69d39dce5542123
Tyrese Mathenlall <Tyrese.Mathenlall@gppos.net>
2025-10-17 19:08:00 +0200

    Removed choices variable

--- life.py
+++ life.py
@@ -3,14 +3,13 @@ import random
 def random_state(width, height):
     board1 = []
     board2 = []
-    dead_alive = [0,1]
 
     for _ in range(width):
-        value = random.choice(dead_alive)
+        value = random.choice([0,1])
         board1.append(value)
         board2.append(board1)
 
     return board2
 
 
-
+print(random_state(5,5))

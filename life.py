96f7eaa655f81971e33c71f2bf81680343846f8d
Tyrese Mathenlall <Tyrese.Mathenlall@gppos.net>
2025-10-17 18:49:54 +0200

    Made dead state

--- life.py
+++ life.py
@@ -1,17 +1,20 @@
-def dead_state(width, height=0):
+def dead_state(width, height):
     board1 = []
     board2 = []
 
     # For width
     for _ in range(width):
         board1.append(0)
+
+
+    for _ in range(height):
         board2.append(board1)
 
     return board2
 
 
     
-print(dead_state(5))
+print(dead_state(2,2))
 
     # [
     # [0,0,0,0,0], 

372736641189855f1a94f2f8aa6020693ff83717
Tyrese6553 <tmathenlall@gmail.com>
2025-10-17 20:43:27 +0200

    2nd correction on random_state function to ensure random sublists

--- life.py
+++ life.py
@@ -1,37 +1,16 @@
 import random
 
 def random_state(width, height):
-    board1 = []
     board2 = []
 
-    for _ in range(width):
-        value = random.choice([0,1])
-        board1.append(value)
-   
     for _ in range(height):
+        # Initialises board1 again so it resets the values in the list
+        # This allows each sublist to be random and not the same set each time
+        board1 = []
+        for _ in range(width):
+            value = random.choice([0,1])
+            board1.append(value)
         board2.append(board1)
 
     return board2
 
-
-
-'''
-def render(board):
-    new_board1 = []
-    new_board2 = []
-
-    for num in board:
-        for nest in num:
-            if nest == 0:
-                new_board1.append(" ")
-            if nest == 1:
-                new_board1.append("#")
-
-    return new_board1
-
-board = [[1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0]]
-
-print(render(board))
-'''
-
-    # use \n to print new line after each element from list
